d7562dfec465282e5d02c47a282e7890a5220197
Pallavi Kadimi <spallavikadimi@gmail.com>
2026-04-07 15:26:44 +0530

    Added HomeController

---
+++ src/main/java/com/project/clms/HomeController.java
@@ -0,0 +1,13 @@
+package com.project.clms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Cloud Log Monitoring System is running successfully";
+    }
+}
