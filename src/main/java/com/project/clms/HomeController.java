8cc3ed67fbe787d2561c1d6bd68a50b619019395
Pallavi Kadimi <spallavikadimi@gmail.com>
2026-04-07 17:56:03 +0530

    Final dashboard setup for Railway deployment

--- src/main/java/com/project/clms/HomeController.java
+++ src/main/java/com/project/clms/HomeController.java
@@ -1,13 +1,13 @@
-package com.project.clms;
+package com.project.clms.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "dashboard";
+        return "dashboard"; // looks for dashboard.html in templates
     }
 }
